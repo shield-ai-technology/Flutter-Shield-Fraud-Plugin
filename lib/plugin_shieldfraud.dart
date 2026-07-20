58ecc5c8d73460a90a354cd6f83ee4de5637ca1b
Sheril Philip <sherilphilip@admins-Laptop.local>
2026-07-20 16:18:08 +0530

    [MT-922]: Update Flutter to use iOS v2.0.0

--- lib/plugin_shieldfraud.dart
+++ lib/plugin_shieldfraud.dart
@@ -49,12 +49,6 @@ class Shield {
         args["blockScreenRecording"] = config.blockScreenRecording;
       }
 
-      // iOS 1.x.x
-      if (Platform.isIOS) {
-        args["enableBackgroundListener"] = config.enableBackgroundListener;
-        args["enableMocking"] = config.enableMocking;
-      }
-
       await _channel.invokeMethod("initShieldFraud", args);
     } catch (e, s) {
       _internalLog("initShield failed", e, s);
@@ -147,7 +141,7 @@ class Shield {
       Map<String, String> data,
       ) async {
     try {
-      final result = await _channel.invokeMethod(
+      final String? result = await _channel.invokeMethod<String>(
         "sendAttributes",
         {
           "screenName": screenName,
@@ -156,18 +150,7 @@ class Shield {
       );
 
       latestError = null;
-
-      // iOS old SDK → bool
-      if (result is bool) {
-        if (!result) {
-          latestError = const ShieldError("0", "sendAttributes failed");
-          return null;
-        }
-        return await sessionId;
-      }
-
-      // Android new SDK → sessionId
-      return result?.toString();
+      return result;
     } on PlatformException catch (e, s) {
       _internalLog("sendAttributes failed", e, s);
       latestError = ShieldError(
@@ -205,8 +188,8 @@ class Shield {
         args["userId"] = userId;
       }
 
-      final result = await _channel
-          .invokeMethod(
+      final String? result = await _channel
+          .invokeMethod<String>(
         "sendDeviceSignature",
         args,
       )
@@ -223,16 +206,7 @@ class Shield {
         return null;
       }
 
-      // iOS old SDK → bool
-      if (result is bool) {
-        if (!result) {
-          latestError = const ShieldError("0", "sendDeviceSignature failed");
-          return null;
-        }
-        return await sessionId;
-      }
-
-      return result?.toString();
+      return result;
     } on PlatformException catch (e, s) {
       _internalLog("sendDeviceSignature failed", e, s);
       latestError = ShieldError(
