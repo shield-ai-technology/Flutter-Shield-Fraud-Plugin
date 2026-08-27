7102ef830fe77a278215693f382fb13ee8c2ae43
IanWang <ian.wang@shield.com>
2026-08-27 22:36:29 +0800

    feat: bump version to v2.4.2

--- lib/generated/plugin_version_info.dart
+++ lib/generated/plugin_version_info.dart
@@ -1,6 +1,6 @@
   /// This file is generated automatically. Do not edit manually.
   class PluginBuildInfo {
-    static const String pluginVersion = '2.4.1';
+    static const String pluginVersion = '2.4.2';
     static const String pluginName = 'flutter_shieldalpha';
   }
   
